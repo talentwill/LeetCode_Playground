8fe91f73781697b8c97ce3658be6730cdc1d7dbf
talentwill <talentwill@outlook.com>
2024-04-24 15:42:59 +0800

    Modify configs.

--- common/cpplang.h
+++ common/cpplang.h
@@ -1,2 +1,18 @@
+#if defined(__linux__)
 #include <bits/stdc++.h>
+#else
+
+#include <algorithm>
+#include <atomic>
+#include <cstddef>
+#include <iostream>
+#include <memory>
+#include <mutex>
+#include <numeric>
+#include <string>
+#include <thread>
+#include <vector>
+
+#endif
+
 using namespace std;
